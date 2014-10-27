a8df90cfded6542e10ee34d75458e1e952f9e63f
romanfox <romanfox8@gmail.com>
2014-10-27 14:18:59 +0200

    hbnContainer

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -2,8 +2,15 @@ package com.controller;
 
 //import org.springframework.stereotype.Controller;
 
+import com.model.User;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
 import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.hbnutil.HbnContainer;
+import com.vaadin.data.util.IndexedContainer;
+import com.vaadin.event.FieldEvents;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
@@ -22,10 +29,11 @@ public class UserController extends UI {
 
     }
 
-    private Table contactList = new Table();
+    private Table userList = new Table();
     private TextField searchField = new TextField();
-    private Button addNewContactButton = new Button("New");
-    private Button removeContactButton = new Button("Remove this contact");
+    private Button addNewUserButton = new Button("New");
+    private Button removeUserButton = new Button("Remove");
+    private Button saveUserButton = new Button("Save");
     private FormLayout editorLayout = new FormLayout();
     private FieldGroup editorFields = new FieldGroup();
 
@@ -38,13 +46,21 @@ public class UserController extends UI {
     private static final String[] fieldNames = new String[] {USER_ID, NAME,
             AGE, IS_ADMIN, CREATED_DATE};
 
+    private Table table;
+    /*
+         * Any component can be bound to an external data source. This example uses
+         * just a dummy in-memory list, but there are many more practical
+         * implementations.
+         */
+    IndexedContainer contactContainer = table.setContainerDataSource(new HbnContainer(User.class, this));
+
     @Override
     protected void init(VaadinRequest request) {
         initLayout();
-//        initContactList();
-//        initEditor();
-//        initSearch();
-//        initAddRemoveButtons();
+        initContactList();
+        initEditor();
+        initSearch();
+        initAddRemoveButtons();
     }
 //    protected void init(VaadinRequest request) {
 //        final VerticalLayout layout = new VerticalLayout();
@@ -107,25 +123,25 @@ public class UserController extends UI {
         VerticalLayout leftLayout = new VerticalLayout();
         splitPanel.addComponent(leftLayout);
         splitPanel.addComponent(editorLayout);
-        leftLayout.addComponent(contactList);
+        leftLayout.addComponent(userList);
         HorizontalLayout bottomLeftLayout = new HorizontalLayout();
         leftLayout.addComponent(bottomLeftLayout);
         bottomLeftLayout.addComponent(searchField);
-        bottomLeftLayout.addComponent(addNewContactButton);
+        bottomLeftLayout.addComponent(addNewUserButton);
 
 		/* Set the contents in the left of the split panel to use all the space */
         leftLayout.setSizeFull();
 
 		/*
-		 * On the left side, expand the size of the contactList so that it uses
+		 * On the left side, expand the size of the userList so that it uses
 		 * all the space left after from bottomLeftLayout
 		 */
-        leftLayout.setExpandRatio(contactList, 1);
-        contactList.setSizeFull();
+        leftLayout.setExpandRatio(userList, 1);
+        userList.setSizeFull();
 
 		/*
 		 * In the bottomLeftLayout, searchField takes all the width there is
-		 * after adding addNewContactButton. The height of the layout is defined
+		 * after adding addNewUserButton. The height of the layout is defined
 		 * by the tallest component.
 		 */
         bottomLeftLayout.setWidth("100%");
@@ -137,4 +153,171 @@ public class UserController extends UI {
         editorLayout.setVisible(false);
     }
 
+    private void initEditor() {
+
+        editorLayout.addComponent(removeUserButton);
+
+		/* User interface can be created dynamically to reflect underlying data. */
+        for (String fieldName : fieldNames) {
+            TextField field = new TextField(fieldName);
+            editorLayout.addComponent(field);
+            field.setWidth("100%");
+
+			/*
+			 * We use a FieldGroup to connect multiple components to a data
+			 * source at once.
+			 */
+            editorFields.bind(field, fieldName);
+        }
+
+		/*
+		 * Data can be buffered in the user interface. When doing so, commit()
+		 * writes the changes to the data source. Here we choose to write the
+		 * changes automatically without calling commit().
+		 */
+        editorFields.setBuffered(false);
+    }
+
+    private void initAddRemoveButtons() {
+        addNewUserButton.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+
+				/*
+				 * Rows in the Container data model are called Item. Here we add
+				 * a new row in the beginning of the list.
+				 */
+                contactContainer.removeAllContainerFilters();
+                Object contactId = contactContainer.addItemAt(0);
+
+				/*
+				 * Each Item has a set of Properties that hold values. Here we
+				 * set a couple of those.
+				 */
+                userList.getContainerProperty(contactId, USER_ID).setValue(
+                        "New");
+                userList.getContainerProperty(contactId, NAME).setValue(
+                        "Name");
+
+				/* Lets choose the newly created contact to edit it. */
+                userList.select(contactId);
+            }
+        });
+
+        removeUserButton.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                Object userId = userList.getValue();
+                userList.removeItem(userId);
+            }
+        });
+    }
+
+    private void initSearch() {
+
+		/*
+		 * We want to show a subtle prompt in the search field. We could also
+		 * set a caption that would be shown above the field or description to
+		 * be shown in a tooltip.
+		 */
+        searchField.setInputPrompt("Search contacts");
+
+		/*
+		 * Granularity for sending events over the wire can be controlled. By
+		 * default simple changes like writing a text in TextField are sent to
+		 * server with the next Ajax call. You can set your component to be
+		 * immediate to send the changes to server immediately after focus
+		 * leaves the field. Here we choose to send the text over the wire as
+		 * soon as user stops writing for a moment.
+		 */
+        searchField.setTextChangeEventMode(AbstractTextField.TextChangeEventMode.LAZY);
+
+		/*
+		 * When the event happens, we handle it in the anonymous inner class.
+		 * You may choose to use separate controllers (in MVC) or presenters (in
+		 * MVP) instead. In the end, the preferred application architecture is
+		 * up to you.
+		 */
+        searchField.addTextChangeListener(new FieldEvents.TextChangeListener() {
+            public void textChange(final FieldEvents.TextChangeEvent event) {
+
+				/* Reset the filter for the contactContainer. */
+                contactContainer.removeAllContainerFilters();
+                contactContainer.addContainerFilter(new ContactFilter(event
+                        .getText()));
+            }
+        });
+    }
+
+    private void initContactList() {
+        userList.setContainerDataSource(contactContainer);
+        userList.setVisibleColumns(new String[]{USER_ID, NAME, AGE});
+        userList.setSelectable(true);
+        userList.setImmediate(true);
+
+        userList.addValueChangeListener(new Property.ValueChangeListener() {
+            public void valueChange(Property.ValueChangeEvent event) {
+                Object contactId = userList.getValue();
+
+				/*
+				 * When a contact is selected from the list, we want to show
+				 * that in our editor on the right. This is nicely done by the
+				 * FieldGroup that binds all the fields to the corresponding
+				 * Properties in our contact at once.
+				 */
+                if (contactId != null)
+                    editorFields.setItemDataSource(userList
+                            .getItem(contactId));
+
+                editorLayout.setVisible(contactId != null);
+            }
+        });
+    }
+
+    private class ContactFilter implements Container.Filter {
+        private String needle;
+
+        public ContactFilter(String needle) {
+            this.needle = needle.toLowerCase();
+        }
+
+        public boolean passesFilter(Object itemId, Item item) {
+            String haystack = ("" + item.getItemProperty(USER_ID).getValue()
+                    + item.getItemProperty(NAME).getValue() + item
+                    .getItemProperty(AGE).getValue()).toLowerCase();
+            return haystack.contains(needle);
+        }
+
+        public boolean appliesToProperty(Object id) {
+            return true;
+        }
+    }
+
+    /*
+	 * Generate some in-memory example data to play with. In a real application
+	 * we could be using SQLContainer, JPAContainer or some other to persist the
+	 * data.
+	 */
+    private static IndexedContainer createDummyDatasource() {
+        IndexedContainer ic = new IndexedContainer();
+
+        for (String p : fieldNames) {
+            ic.addContainerProperty(p, String.class, "");
+        }
+
+		/* Create dummy data by randomly combining first and last names */
+        String[] fnames = { "Peter", "Alice", "Joshua", "Mike", "Olivia",
+                "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene",
+                "Lisa", "Marge" };
+        String[] lnames = { "Smith", "Gordon", "Simpson", "Brown", "Clavel",
+                "Simons", "Verne", "Scott", "Allison", "Gates", "Rowling",
+                "Barks", "Ross", "Schneider", "Tate" };
+        for (int i = 0; i < 100; i++) {
+            Object id = ic.addItem();
+            ic.getContainerProperty(id, USER_ID).setValue(
+                    fnames[(int) (fnames.length * Math.random())]);
+            ic.getContainerProperty(id, NAME).setValue(
+                    lnames[(int) (lnames.length * Math.random())]);
+        }
+
+        return ic;
+    }
 }
