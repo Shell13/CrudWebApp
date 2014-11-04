640e0ba6c5a90f03fbcdf0c449c75dba75574b92
romanfox <romanfox8@gmail.com>
2014-11-04 08:36:22 +0200

    sql schema

---
+++ src/main/resources/createUserTable.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+  id int(8) unsigned NOT NULL AUTO_INCREMENT,
+  name varchar(25) NOT NULL,
+  age int(3) unsigned NOT NULL,
+  isAdmin bit(1) DEFAULT b'0',
+  createdDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=19223 DEFAULT CHARSET=utf8;
